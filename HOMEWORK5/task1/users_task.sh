a130ed9f7db32340ae815ab7c31f8512f4e65f31
vlad <vlad19972017@gmail.com>
2024-03-01 18:12:18 +0200

    Update content

--- HOMEWORK5/task1/users_task.sh
+++ HOMEWORK5/task1/users_task.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# Create User Accounts
-useradd dev1
-useradd dev2
-useradd dev3
-useradd backupdev
+# Create Users
+#We can also use `adduser`` like more user-friendly.
+
+sudo useradd dev1
+sudo useradd dev2
+sudo useradd dev3
+sudo useradd backupdev
 
 # Create Groups
-groupadd developers
-groupadd webmasters
+sudo groupadd developers
+sudo groupadd webmasters
 
 # Assign Users to Groups
-usermod -aG developers dev1
-usermod -aG developers dev2
-usermod -aG webmasters dev3
+sudo usermod -aG developers dev1
+sudo usermod -aG developers dev2
+sudo usermod -aG webmasters dev3
 
 # Set Default Group for Users
-usermod -g developers dev1
-usermod -g developers dev2
+sudo usermod -g developers dev1
+sudo usermod -g developers dev2
 
 # Create Home Directories
 mkdir /home/dev1
@@ -25,15 +27,18 @@ mkdir /home/dev2
 mkdir /home/dev3
 mkdir /home/backupdev
 
-# Clone a User Account
-rsync -a /home/dev1/ /home/backupdev/
+# Copy a User dev1 to new file /home/backupdev
+#We can also use cp, but i like rsync.
+sudo rsync -a /home/dev1/ /home/backupdev/
 
 # Set Permissions for a Shared Project
 mkdir /home/web_project
-chgrp developers /home/web_project
-chmod g+rw /home/web_project
+sudo chgrp developers /home/web_project
+sudo chmod g+rw /home/web_project
 
 # Immutable log file
 touch /home/my.log
-chmod 644 /home/my.log
-chattr +a /home/my.log
+sudo chmod 644 /home/my.log
+sudo chattr +a /home/my.log
+
+sudo chmod +t /home/test_user1 # just for testing sticky bit
