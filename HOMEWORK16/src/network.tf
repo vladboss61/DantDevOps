bb3a0efd19a9e0a34491d7f9b6ec8e18250a2b9d
Vladyslav Varvashenko <vladyslav.varvashenko@stryker.com>
2024-07-15 14:02:22 +0300

    Add comments for nat

--- HOMEWORK16/src/network.tf
+++ HOMEWORK16/src/network.tf
@@ -22,3 +22,35 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+
+# Create NAT Gateway in public subnet
+# resource "aws_nat_gateway" "nat_gateway" {
+#   allocation_id = aws_eip.nat_eip.id
+#   subnet_id     = aws_subnet.public.id
+# }
+
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.main.id
+  
+#   tags = {
+#     Name = "private-route-table"
+#   }
+# }
+
+# resource "aws_eip" "nat_eip" {
+#   vpc = true
+# }
+
+# # Modify private subnet route table to route traffic through NAT Gateway
+# resource "aws_route" "private_subnet_route" {
+#   route_table_id            = aws_route_table.private.id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   nat_gateway_id            = aws_nat_gateway.nat_gateway.id
+# }
+
+# # Associate private subnet with modified route table
+# resource "aws_route_table_association" "private_subnet_association" {
+#   subnet_id      = aws_subnet.private.id
+#   route_table_id = aws_route_table.private.id
+# }
