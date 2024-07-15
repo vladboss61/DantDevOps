9a5611d28e32a4737cf6bbf79e7c9bdcde7303e9
Vladyslav Varvashenko <vladyslav.varvashenko@stryker.com>
2024-07-15 13:48:50 +0300

    Updates.

--- HOMEWORK16/src/outputs.tf
+++ HOMEWORK16/src/outputs.tf
@@ -7,6 +7,5 @@ output "private_instance_private_ip" {
 }
 
 output "private_key_pem" {
-  value     = tls_private_key.example.private_key_pem
-  sensitive = true
+  value = tls_private_key.example.private_key_pem
 }
